d580005d9f106703a11ed7768d057fc7148f22a7
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-09-03 20:43:10 +0300

    Add template

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     #[Route('/hello/{name}', name: 'index')]
     public function index(string $name): Response
     {
-        return new Response("Hello $name!");
+        return $this->render('default/index.html.twig', [
+            'name' => $name,
+        ]);
     }
 
     #[Route('/simplicity', methods: ['GET'])]
