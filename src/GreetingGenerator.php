ff80550d1d38b890052b8d1e11965f4a136314d9
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-09-05 13:08:16 +0300

    Add logger to service

--- src/GreetingGenerator.php
+++ src/GreetingGenerator.php
@@ -2,12 +2,20 @@
 
 namespace App;
 
+use Psr\Log\LoggerInterface;
+
 class GreetingGenerator
 {
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
     public function getRandomGreeting(): string
     {
         $greetings = ['Hey', 'Yo', 'Aloha'];
         $greeting = $greetings[array_rand($greetings)];
+
+        $this->logger->info('Using the greeting:' . $greeting);
 
         return $greeting;
     }
