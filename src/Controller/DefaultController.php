2c21fcbbea38088a5e4ff7518026ab3ed9a21fc5
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-09-05 15:29:08 +0300

    small fix

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\GreetingGenerator;
-use App\Twig\GreetExtension;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
