5e30a13c366168d20aa4289c91f7599174a36137
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-09-05 11:57:35 +0300

    fix import ApiResource

---
+++ src/Entity/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
+#[ORM\Entity]
+#[ApiResource]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string')]
+    private string $name;
+
+    #[ORM\Column(type: 'integer')]
+    private int $price;
+
+    // ...
+}
