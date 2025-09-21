67fb08514dd0cfdc9d28d5a6a74a384c229db877
Maxim_10407 <berezovskiyma1@transport.mos.ru>
2025-09-21 15:39:33 +0300

    controller: get services

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class LuckyController extends AbstractController
 {
-    #[Route('/lucky/number')]
-    public function number(): Response
+    #[Route(path: '/lucky/number/{max}', name: 'app_lucky_number')]
+    public function number(int $max, LoggerInterface $logger): Response
     {
-        $number = random_int(0, 100);
+        $number = random_int(0, $max);
+        $url = $this->generateUrl('app_lucky_number', ['max' => $max]);
+        $logger->info('We are logging: ' . $url);
+
 
         return $this->render('lucky/number.html.twig', [
             'number' => $number,
+            'generated_url' => $url
         ]);
+
     }
 }
